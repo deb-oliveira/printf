78e3ed01e09320ee90fb4b9acd0834fd6da58385
dde-oliv <doliveira@doliveira.localdomain>
2021-06-15 14:43:04 -0300

    added %n case

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: doliveira <doliveira@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/11 14:59:47 by doliveira         #+#    #+#             */
-/*   Updated: 2021/06/15 10:51:32 by doliveira        ###   ########.fr       */
+/*   Updated: 2021/06/15 14:35:56 by doliveira        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ static void	do_print(char *str_cpy, char **print, t_specf *specf, int fd)
 	else
 		ft_putmem_fd(*print, 1, fd);
 	free(*print);
-	free(specf->flags);
 }
 
 int	ft_printf(const char *str, ...)
@@ -40,13 +39,19 @@ int	ft_printf(const char *str, ...)
 	{
 		str_cpy++;
 		get_specf(&str_cpy, &specf, &arg);
-		get_type(str_cpy, &print, specf, &arg);
-		do_specf(str_cpy, specf, &print);
-		if (*str_cpy != 'c')
-			count += ft_strlen(print);
+		if (*str_cpy == 'n')
+			*(va_arg(arg, int *)) = count;
 		else
-			count += ft_max(1, specf.width);
-		do_print(str_cpy, &print, &specf, 1);
+		{
+			get_type(str_cpy, &print, specf, &arg);
+			do_specf(str_cpy, specf, &print);
+			if (*str_cpy != 'c')
+				count += ft_strlen(print);
+			else
+				count += ft_max(1, specf.width);
+			do_print(str_cpy, &print, &specf, 1);
+		}
+		free(specf.flags);
 		str_cpy = ft_putcstr_fd((char *)(str_cpy + 1), '%', 1, &count);
 	}
 	va_end(arg);
