bff7e3c8aabc5b706ee83157d1edd06342e3e890
dde-oliv <doliveira@doliveira.localdomain>
2021-07-03 08:20:42 -0300

    dealing with overflow; lenght ll and l

--- ft_litoa_base.c
+++ ft_litoa_base.c
@@ -6,7 +6,7 @@
 /*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 17:52:22 by dde-oliv          #+#    #+#             */
-/*   Updated: 2021/07/02 17:52:50 by dde-oliv         ###   ########.fr       */
+/*   Updated: 2021/07/03 08:16:14 by dde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,24 @@
 
 char	*ft_litoa_base(long int nbr, char *base)
 {
-	unsigned int	nbr_aux;
 	char			*str;
 	char			*str_aux;
+	int				size;
 
-	nbr_aux = 0;
 	if (nbr >= 0)
 		return (ft_lutoa_base((long unsigned int)nbr, base));
-	nbr_aux = (long unsigned int)(-1 * (nbr + 1)) + 1;
-	str = ft_lutoa_base(nbr_aux, base);
-	str_aux = str;
-	str = ft_strjoin("-", str);
-	free(str_aux);
+	size = ft_strlen(base);
+	str = ft_strdup("-");
+	while (nbr != 0)
+	{
+		str_aux = str;
+		str = ft_strcjoin(str, base[-(nbr % -size)]);
+		free(str_aux);
+		nbr = nbr / size;
+	}
+	str++;
+	ft_strrev(&str);
+	str--;
 	return (str);
 }
+
--- ft_llitoa_base.c
+++ ft_llitoa_base.c
@@ -6,7 +6,7 @@
 /*   By: dde-oliv <dde-oliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 17:53:06 by dde-oliv          #+#    #+#             */
-/*   Updated: 2021/07/02 17:53:40 by dde-oliv         ###   ########.fr       */
+/*   Updated: 2021/07/03 08:18:52 by dde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,23 @@
 
 char	*ft_llitoa_base(long long int nbr, char *base)
 {
-	unsigned int	nbr_aux;
+	int				size;
 	char			*str;
 	char			*str_aux;
 
-	nbr_aux = 0;
 	if (nbr >= 0)
 		return (ft_llutoa_base((long long unsigned int)nbr, base));
-	nbr_aux = (long long unsigned int)(-1 * (nbr + 1)) + 1;
-	str = ft_llutoa_base(nbr_aux, base);
-	str_aux = str;
-	str = ft_strjoin("-", str);
-	free(str_aux);
+	size = ft_strlen(base);
+	str = ft_strdup("-");
+	while (nbr != 0)
+	{
+		str_aux = str;
+		str = ft_strcjoin(str, base[-(nbr % -size)]);
+		free(str_aux);
+		nbr = nbr / size;
+	}
+	str++;
+	ft_strrev(&str);
+	str--;
 	return (str);
 }
